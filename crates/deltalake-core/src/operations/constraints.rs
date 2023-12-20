36b8821910af6087d73dfc4341c0f05e072ef2f3
Stephen Carman <stephen.carman@databricks.com>
2023-12-20 14:10:33 -0500

    feat: check constraints, added the ability to include multiple constraints at once

--- crates/deltalake-core/src/operations/constraints.rs
+++ crates/deltalake-core/src/operations/constraints.rs
@@ -32,8 +32,7 @@ use super::datafusion_utils::into_expr;
 /// Build a constraint to add to a table
 pub struct ConstraintBuilder {
     snapshot: DeltaTableState,
-    name: Option<String>,
-    expr: Option<Expression>,
+    constraints: Vec<(String, Expression)>,
     log_store: LogStoreRef,
     state: Option<SessionState>,
 }
@@ -42,22 +41,30 @@ impl ConstraintBuilder {
     /// Create a new builder
     pub fn new(log_store: LogStoreRef, snapshot: DeltaTableState) -> Self {
         Self {
-            name: None,
-            expr: None,
+            constraints: vec![],
             snapshot,
             log_store,
             state: None,
         }
     }
 
-    /// Specify the constraint to be added
+    /// Specify a constraint to be added
     pub fn with_constraint<S: Into<String>, E: Into<Expression>>(
         mut self,
         name: S,
         expression: E,
     ) -> Self {
-        self.name = Some(name.into());
-        self.expr = Some(expression.into());
+        self.constraints.push((name.into(), expression.into()));
+        self
+    }
+
+    /// Specify multiple constraints to be added. These will be added in the order they are in the vec
+    pub fn with_constraints<S: Into<String>, E: Into<Expression>>(
+        mut self,
+        constraints: Vec<(S, E)>,
+    ) -> Self {
+        let build_constraints: Vec<_> = constraints.into_iter().map(|(k, v)| (k.into(), v.into())).collect();
+        self.constraints.extend(build_constraints);
         self
     }
 
@@ -77,27 +84,10 @@ impl std::future::IntoFuture for ConstraintBuilder {
         let mut this = self;
 
         Box::pin(async move {
-            let name = match this.name {
-                Some(v) => v,
-                None => return Err(DeltaTableError::Generic("No name provided".to_string())),
-            };
-
-            let expr = this
-                .expr
-                .ok_or_else(|| DeltaTableError::Generic("No Expresion provided".to_string()))?;
-
-            let mut metadata = this
-                .snapshot
-                .metadata()
-                .ok_or(DeltaTableError::NoMetadata)?
-                .clone();
-            let configuration_key = format!("delta.constraints.{}", name);
-
-            if metadata.configuration.contains_key(&configuration_key) {
-                return Err(DeltaTableError::Generic(format!(
-                    "Constraint with name: {} already exists",
-                    name
-                )));
+            if this.constraints.is_empty() {
+                return Err(DeltaTableError::Generic(
+                    "No Constraints Provided".to_string(),
+                ));
             }
 
             let state = this.state.unwrap_or_else(|| {
@@ -106,105 +96,123 @@ impl std::future::IntoFuture for ConstraintBuilder {
                 session.state()
             });
 
-            let scan = DeltaScanBuilder::new(&this.snapshot, this.log_store.clone(), &state)
-                .build()
+            for (name, expr) in this.constraints {
+                let scan = DeltaScanBuilder::new(&this.snapshot, this.log_store.clone(), &state)
+                    .build()
+                    .await?;
+
+                let schema = scan.schema().to_dfschema()?;
+
+                let mut metadata = this
+                    .snapshot
+                    .metadata()
+                    .ok_or(DeltaTableError::NoMetadata)?
+                    .clone();
+
+                let configuration_key = format!("delta.constraints.{}", name);
+                if metadata.configuration.contains_key(&configuration_key) {
+                    return Err(DeltaTableError::Generic(format!(
+                        "Constraint with name: {} already exists",
+                        name
+                    )));
+                }
+
+                let expr = into_expr(expr, &schema, &state)?;
+                let expr_str = fmt_expr_to_sql(&expr)?;
+
+                // Checker built here with the one time constraint to check.
+                let checker =
+                    DeltaDataChecker::new_with_constraints(vec![Constraint::new("*", &expr_str)]);
+
+                let plan: Arc<dyn ExecutionPlan> = Arc::new(scan);
+                let mut tasks = vec![];
+                for p in 0..plan.output_partitioning().partition_count() {
+                    let inner_plan = plan.clone();
+                    let inner_checker = checker.clone();
+                    let task_ctx = Arc::new(TaskContext::from(&state));
+                    let mut record_stream: SendableRecordBatchStream =
+                        inner_plan.execute(p, task_ctx)?;
+                    let handle: tokio::task::JoinHandle<DeltaResult<()>> =
+                        tokio::task::spawn(async move {
+                            while let Some(maybe_batch) = record_stream.next().await {
+                                let batch = maybe_batch?;
+                                inner_checker.check_batch(&batch).await?;
+                            }
+                            Ok(())
+                        });
+                    tasks.push(handle);
+                }
+                futures::future::join_all(tasks)
+                    .await
+                    .into_iter()
+                    .collect::<Result<Vec<_>, _>>()
+                    .map_err(|err| DeltaTableError::Generic(err.to_string()))?
+                    .into_iter()
+                    .collect::<Result<Vec<_>, _>>()?;
+
+                // We have validated the table passes it's constraints, now to add the constraint to
+                // the table.
+
+                metadata.configuration.insert(
+                    format!("delta.constraints.{}", name),
+                    Some(expr_str.clone()),
+                );
+
+                let old_protocol = this.snapshot.protocol();
+                let protocol = Protocol {
+                    min_reader_version: if old_protocol.min_reader_version > 1 {
+                        old_protocol.min_reader_version
+                    } else {
+                        1
+                    },
+                    min_writer_version: if old_protocol.min_writer_version > 3 {
+                        old_protocol.min_writer_version
+                    } else {
+                        3
+                    },
+                    reader_features: old_protocol.reader_features.clone(),
+                    writer_features: old_protocol.writer_features.clone(),
+                };
+
+                let operational_parameters = HashMap::from_iter([
+                    ("name".to_string(), json!(&name)),
+                    ("expr".to_string(), json!(&expr_str)),
+                ]);
+
+                let operations = DeltaOperation::AddConstraint {
+                    name: name.clone(),
+                    expr: expr_str.clone(),
+                };
+
+                let commit_info = CommitInfo {
+                    timestamp: Some(Utc::now().timestamp_millis()),
+                    operation: Some(operations.name().to_string()),
+                    operation_parameters: Some(operational_parameters),
+                    read_version: Some(this.snapshot.version()),
+                    isolation_level: Some(IsolationLevel::Serializable),
+                    is_blind_append: Some(false),
+                    ..Default::default()
+                };
+
+                let actions = vec![
+                    Action::CommitInfo(commit_info),
+                    Action::Metadata(Metadata::try_from(metadata)?),
+                    Action::Protocol(protocol),
+                ];
+
+                let version = commit(
+                    this.log_store.as_ref(),
+                    &actions,
+                    operations,
+                    &this.snapshot,
+                    None,
+                )
                 .await?;
 
-            let schema = scan.schema().to_dfschema()?;
-            let expr = into_expr(expr, &schema, &state)?;
-            let expr_str = fmt_expr_to_sql(&expr)?;
-
-            // Checker built here with the one time constraint to check.
-            let checker =
-                DeltaDataChecker::new_with_constraints(vec![Constraint::new("*", &expr_str)]);
-
-            let plan: Arc<dyn ExecutionPlan> = Arc::new(scan);
-            let mut tasks = vec![];
-            for p in 0..plan.output_partitioning().partition_count() {
-                let inner_plan = plan.clone();
-                let inner_checker = checker.clone();
-                let task_ctx = Arc::new(TaskContext::from(&state));
-                let mut record_stream: SendableRecordBatchStream =
-                    inner_plan.execute(p, task_ctx)?;
-                let handle: tokio::task::JoinHandle<DeltaResult<()>> =
-                    tokio::task::spawn(async move {
-                        while let Some(maybe_batch) = record_stream.next().await {
-                            let batch = maybe_batch?;
-                            inner_checker.check_batch(&batch).await?;
-                        }
-                        Ok(())
-                    });
-                tasks.push(handle);
+                this.snapshot
+                    .merge(DeltaTableState::from_actions(actions, version)?, true, true);
             }
-            futures::future::join_all(tasks)
-                .await
-                .into_iter()
-                .collect::<Result<Vec<_>, _>>()
-                .map_err(|err| DeltaTableError::Generic(err.to_string()))?
-                .into_iter()
-                .collect::<Result<Vec<_>, _>>()?;
 
-            // We have validated the table passes it's constraints, now to add the constraint to
-            // the table.
-
-            metadata.configuration.insert(
-                format!("delta.constraints.{}", name),
-                Some(expr_str.clone()),
-            );
-
-            let old_protocol = this.snapshot.protocol();
-            let protocol = Protocol {
-                min_reader_version: if old_protocol.min_reader_version > 1 {
-                    old_protocol.min_reader_version
-                } else {
-                    1
-                },
-                min_writer_version: if old_protocol.min_writer_version > 3 {
-                    old_protocol.min_writer_version
-                } else {
-                    3
-                },
-                reader_features: old_protocol.reader_features.clone(),
-                writer_features: old_protocol.writer_features.clone(),
-            };
-
-            let operational_parameters = HashMap::from_iter([
-                ("name".to_string(), json!(&name)),
-                ("expr".to_string(), json!(&expr_str)),
-            ]);
-
-            let operations = DeltaOperation::AddConstraint {
-                name: name.clone(),
-                expr: expr_str.clone(),
-            };
-
-            let commit_info = CommitInfo {
-                timestamp: Some(Utc::now().timestamp_millis()),
-                operation: Some(operations.name().to_string()),
-                operation_parameters: Some(operational_parameters),
-                read_version: Some(this.snapshot.version()),
-                isolation_level: Some(IsolationLevel::Serializable),
-                is_blind_append: Some(false),
-                ..Default::default()
-            };
-
-            let actions = vec![
-                Action::CommitInfo(commit_info),
-                Action::Metadata(Metadata::try_from(metadata)?),
-                Action::Protocol(protocol),
-            ];
-
-            let version = commit(
-                this.log_store.as_ref(),
-                &actions,
-                operations,
-                &this.snapshot,
-                None,
-            )
-            .await?;
-
-            this.snapshot
-                .merge(DeltaTableState::from_actions(actions, version)?, true, true);
             Ok(DeltaTable::new_with_state(this.log_store, this.snapshot))
         })
     }
@@ -284,6 +292,34 @@ mod tests {
             get_constraint(&table, "delta.constraints.id"),
             expected_expr
         );
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn add_multiple_valid_constraint() -> DeltaResult<()> {
+        let batch = get_record_batch(None, false);
+        let write = DeltaOps(create_bare_table())
+            .write(vec![batch.clone()])
+            .await?;
+        let table = DeltaOps(write);
+
+        let table = table
+            .add_constraint()
+            .with_constraints(vec![
+                ("id", "value < 1000"),
+                ("id2", "value > -1000")
+            ])
+            .await?;
+
+        let version = table.version();
+        assert_eq!(version, 2);
+
+        let op_params = &table.metadata()?.configuration;
+        let first_c = op_params.get("delta.constraints.id").unwrap().as_ref().unwrap();
+        let second_c = op_params.get("delta.constraints.id2").unwrap().as_ref().unwrap();
+
+        assert_eq!(first_c, "value < 1000");
+        assert_eq!(second_c, "value > -1000");
         Ok(())
     }
 
